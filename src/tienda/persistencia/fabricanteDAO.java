cf43d8cbc2ed4eae0c4807736e2927dc65085e8c
juans <juans@Juan-Sebastian>
2023-11-14 22:15:42 -0300

    trying making the cod for the "Fabricante" entity.

--- src/tienda/persistencia/fabricanteDAO.java
+++ src/tienda/persistencia/fabricanteDAO.java
@@ -1,6 +1,8 @@
 
 package tienda.persistencia;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import tienda.entidades.Fabricante;
 
 
@@ -31,6 +33,51 @@ public final class fabricanteDAO extends DAO {
         }finally{
             desconectarBase();
      
+        }
+    }
+    public void eliminarFabricante (String nombre) throws Exception{
+        try {
+            String sql = "DELETE FROM fabricante WHERE nombre= '" + nombre + "'";
+            insertarModificarEliminar(sql);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    public Fabricante buscarFabricante (String nombre) throws Exception{
+        try {
+            String sql= "SELECT * FROM fabricante Where nombre='" + nombre + "'";
+            consultarBase(sql);
+            Fabricante fabricante = null;
+            while (resultado.next()) {                
+            fabricante = new Fabricante();
+            fabricante.setidFab(resultado.getInt(1));
+            fabricante.setNombre(resultado.getString(2));
+            }
+            desconectarBase();
+            return fabricante;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    public Collection<Fabricante> listarFabricantes() throws Exception{
+        try {
+            String sql = "Select codigo, nombre FROM producto";
+            consultarBase(sql);
+            Fabricante fabricante = null;
+            Collection <Fabricante> fabricantes = new ArrayList();
+            
+            while (resultado.next()) {                
+            fabricante = new Fabricante();
+            fabricante.setidFab(resultado.getInt(1));
+            fabricante.setNombre(resultado.getString(2));
+            fabricantes.add(fabricante);
+            }
+            desconectarBase();
+            return fabricantes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            desconectarBase();
+            throw new Exception("Error de sistema en la coleccion intetada");
         }
     }
 }
